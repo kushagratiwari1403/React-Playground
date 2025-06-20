33861c5a0a6e4bb156c2a6ba8f8231e3c9dc7bbc
Kushagra Tiwari <tiwarikush414@gmail.com>
2025-06-20 09:39:16 +0530

    Updated code with Functional Component & Component Compiition

---
+++ ep2/App2.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+// JSX - HTML like syntax (not html in javascript)
+
+// const jsxHeading = <h1 style={{color: "green"}}>✅ This is EP 2 JSX Output!</h1>; (React Element)
+
+const JSXHeading = () => {
+    return (
+        <h1 style={{ color: "green" }}>✅ This is EP 2 JSX Output!</h1>
+    )
+};
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+
+// React Components
+
+const HeadingComponent = () => {
+    return <h2> First Way: This is made using React Functional Component </h2>
+};
+
+const Heading = () => <h2> Second Way: This is another made using React Functional Component </h2>;
+
+const HeadingComponent2 = () => (
+    <div id="container">
+        <JSXHeading />
+        <h2 className="Heading2"> Third Way: This is one more another made using React Functional Component </h2>
+    </div>
+);
+
+
+root.render(<HeadingComponent2 />);
